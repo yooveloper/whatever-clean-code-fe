dc85516f48acb70bc30b3907b472304a431af753
yooveloper <yooveloper@gmail.com>
2025-01-10 16:16:52 +0900

    refactor: htmlFor 속성을 활용해 id를 넘겨 레이블과 인풋 연결

--- week-1/src/shared/components/input/Input.tsx
+++ week-1/src/shared/components/input/Input.tsx
@@ -1,20 +1,19 @@
+import { InputHTMLAttributes } from 'react'
 import styles from './Input.module.scss'
 
-type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+type Props = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
 }
 
-function Input({ label, value, onChange, ...rest }: Props) {
+function Input({ label, ...rest }: Props) {
   return (
     <div className={styles.container}>
-      {label && <label className={styles.label}>{label}</label>}
-      <input
-        className={styles.input}
-        type="number"
-        value={value}
-        onChange={onChange}
-        {...rest}
-      />
+      {label && (
+        <label className={styles.label} htmlFor={rest.id}>
+          {label}
+        </label>
+      )}
+      <input className={styles.input} type="number" {...rest} />
     </div>
   )
 }
